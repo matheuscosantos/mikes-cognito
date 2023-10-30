31ec296d1c493245c0e83e2783272d92068eb24c
wagnerlm <wagnerlink@gmail.com>
2023-10-30 20:46:11 -0300

    update bucket s3

--- cognito.tf
+++ cognito.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "mikes-cognito-state"
+    bucket         = "mikes-terraform-state"
     key            = "mikes-cognito.tfstate"
     region         = "us-east-2"
     encrypt        = true
